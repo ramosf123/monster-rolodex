ebaf53b1b0ee90dba0f4dc28cfa5644cc12e34ff
Farid Ramos <framos@ICSMB244.local>
2020-07-01 13:41:19 -0500

    Updates

---
+++ precache-manifest.3085449c028c3cc2a3a10a9a13d0aa0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88788d4585b64d4d206c5c5d1deaade",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5bce0529db1bb8e085b1",
+    "url": "/monster-rolodex/static/css/main.fa9bc9b7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bce0529db1bb8e085b1",
+    "url": "/monster-rolodex/static/js/main.4bbca65c.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
